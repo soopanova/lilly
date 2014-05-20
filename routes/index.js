23f2d830559d10a2abd5463bec80a744e4532b44
System Administrator <root@lappy.local>
2014-05-20 02:10:43 -0400

    added couchdb stuff

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodeCouchDB = require('node-couchdb');
+var couch = new nodeCouchDB('localhost', 5984);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,9 +12,21 @@ router.post('/', function(req, res){
 	var name = req.body.fname;
 	var shorturl = Math.random().toString(36).slice(-8);
 
+
 	html = "<a href='" + name + "'> http://lil.ly/"+ shorturl + "</a>" +
 			"<br> <a href=http://localhost:3000> Back </a>";
 	res.send(html);
+
+	
+	/* ------Get a record
+	couch.get('lilly', 'd82a40cd04f1d4064856c40c7d000906', function(err, resData) {
+		if(err)
+			return console.error(err);
+		console.dir(resData);
+	});*/
+
+	
+
 
 });
 
