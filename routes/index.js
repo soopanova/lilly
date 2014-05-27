910a5f6b3ed335f4b5fa04b7daaf58f9187a8732
lab2112 <brandon.coates@gmail.com>
2014-05-27 01:24:00 -0400

    updating couchDB JSON, still not right

--- routes/index.js
+++ routes/index.js
@@ -12,24 +12,34 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res){
 	var name = req.body.fname;
 	var shorturl = Math.random().toString(36).slice(-8);
-
-
-	html = "<a href='" + name + "'> http://lil.ly/"+ shorturl + "</a>" +
-			"<br> <a href=http://localhost:3000> Back </a>";
-	res.send(html);
-
-	json = "{_id : " + shorturl + ", longURL : " + name + ", shortURL : " + shorturl + "}";
-
-	console.log(json);
-
 	
 
-	/*------Inserting new record
-	db.insert('testdoc', json, function(err, body){
-		if(!err)
-			console.log("something " + body);
-	});
-	*/
+	// html = "<a href='" + name + "'> http://lil.ly/"+ shorturl + "</a>" +
+	// 		"<br> <a href=http://localhost:3000> Back </a>";
+	// res.send(html);
+
+	id = '"_id": "'+shorturl+'"';
+	console.log(id);
+
+	longu = '"longURL": "' + name + '"';
+	console.log(longu);
+
+	//var twoSlash = "\\\\";
+	//console.log(twoSlash);
+	
+	shortu = '"shortURL": "http://lil.ly/' + shorturl + '"';
+	console.log(shortu);
+
+	json = '{' + id + ',' + longu + ',' + shortu + '}';
+	console.log(json);
+
+	//------Inserting new record
+	/*
+	db.insert(json, function(err, body){
+		if(err)
+			console.log("something went wrong: " + err);
+	});*/
+	
 	
 	/* ------Get a record
 	/*couch.get('lilly', 'd82a40cd04f1d4064856c40c7d000906', function(err, resData) {
@@ -37,10 +47,6 @@ router.post('/', function(req, res){
 			return console.error(err);
 		console.dir(resData);
 	});*/
-
-	
-
-
 });
 
 module.exports = router;
