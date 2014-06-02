ac2964a179f2d0e06c86e901dd0966f75c64b699
lab2112 <brandon.coates@gmail.com>
2014-06-01 23:42:48 -0400

    debugging updates

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.post('/', function(req, res){
 router.get(/^\/([a-z]|[0-9]|[A-Z]){8}/, function(req, res){
 	var urlRaw = req.url;
 	var key  = '"' + urlRaw.substr(1,8) + '"';
-	
+	console.log(key);
 	db.get(key,function(err, body){
 		if(err)
 			console.log("something went in the get " + err);
