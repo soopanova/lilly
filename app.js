96e89f62ae7652486e43ab12f8fa0b16daea59ec
lab2112 <brandon.coates@gmail.com>
2014-06-11 23:38:41 -0400

    cleaned up old dependencies

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var pull = require('./routes/pull');
-
-
 var app = express();
 
 // view engine setup
@@ -26,9 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-app.use('/pull', pull);
-
 
 
 /// catch 404 and forwarding to error handler
